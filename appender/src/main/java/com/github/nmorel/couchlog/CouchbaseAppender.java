8039a37882e4003fc26bcbf5ee176f2dfaf16b78
Nicolas Morel <nmr.morel@gmail.com>
2013-04-04 13:00:09 +0200

    persist the MDC, throwable and caller data

--- appender/src/main/java/com/github/nmorel/couchlog/CouchbaseAppender.java
+++ appender/src/main/java/com/github/nmorel/couchlog/CouchbaseAppender.java
@@ -4,11 +4,16 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
+import ch.qos.logback.classic.spi.StackTraceElementProxy;
 import ch.qos.logback.core.UnsynchronizedAppenderBase;
 import com.couchbase.client.CouchbaseClient;
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
@@ -24,32 +29,32 @@ public class CouchbaseAppender
      * the URI list of one or more servers from the cluster
      */
     private List<URI> uriList = new ArrayList<URI>();
-
     /**
      * the bucket name in the cluster you wish to use
      */
     private String bucket;
-
     /**
      * the username for the bucket
      */
     private String user;
-
     /**
      * the password for the bucket
      */
     private String pwd;
-
     /**
-     * @see <a href="http://www.couchbase.com/docs/couchbase-sdk-java-1.0/couchbase-sdk-java-summary-expiry.html">Expiry value</a>.
+     * @see <a href="http://www.couchbase.com/docs/couchbase-sdk-java-1.0/couchbase-sdk-java-summary-expiry
+     *      .html">Expiry value</a>.
      *      0 by default so no expiration.
      */
     private int expiry = 0;
-
     /**
      * Client
      */
     private CouchbaseClient client;
+    /**
+     * If more than one log occurs in the same millisecond, we need something to order them.
+     */
+    private AtomicInteger counter = new AtomicInteger( 0 );
 
     @Override
     public void start()
@@ -89,10 +94,44 @@ public class CouchbaseAppender
         JSONObject log = new JSONObject();
         try
         {
+            log.put( "index", getIndex() );
+            log.put( "timestamp", eventObject.getTimeStamp() );
             log.put( "level", eventObject.getLevel().toInt() );
             log.put( "message", eventObject.getFormattedMessage() );
-            log.put( "timestamp", eventObject.getTimeStamp() );
             log.put( "logger", eventObject.getLoggerName() );
+            log.put( "thread", eventObject.getThreadName() );
+
+            // Throwable
+            if ( null != eventObject.getThrowableProxy() )
+            {
+                JSONArray array = new JSONArray();
+                IThrowableProxy proxy = eventObject.getThrowableProxy();
+                array.put( convertThrowable( proxy ) );
+                while ( null != proxy.getCause() )
+                {
+                    proxy = proxy.getCause();
+                    array.put( convertThrowable( proxy ) );
+                }
+                log.put( "throwable", array );
+            }
+
+            // Caller Data
+            if ( eventObject.hasCallerData() )
+            {
+                // we only store the first one
+                StackTraceElement ste = eventObject.getCallerData()[0];
+                JSONObject steJson = new JSONObject();
+                steJson.put( "class", ste.getClassName() );
+                steJson.put( "method", ste.getMethodName() );
+                steJson.put( "line", ste.getLineNumber() );
+                log.append( "caller", steJson );
+            }
+
+            // MDC
+            for ( Map.Entry<String, String> entry : eventObject.getMDCPropertyMap().entrySet() )
+            {
+                log.put( entry.getKey(), entry.getValue() );
+            }
         }
         catch ( JSONException e )
         {
@@ -100,6 +139,41 @@ public class CouchbaseAppender
         }
 
         client.add( UUID.randomUUID().toString(), expiry, log.toString() );
+    }
+
+    private int getIndex()
+    {
+        for (; ; )
+        {
+            int current = counter.get();
+            // if we reach the max value, we start over to 0. We shouldn't have Integer.MAX_VALUE logs in the same
+            // millisecond
+            int next = current == Integer.MAX_VALUE ? 0 : current + 1;
+            if ( counter.compareAndSet( current, next ) )
+            {
+                return current;
+            }
+        }
+    }
+
+    private JSONObject convertThrowable( IThrowableProxy throwableProxy ) throws JSONException
+    {
+        JSONObject throwable = new JSONObject();
+        throwable.put( "name", throwableProxy.getClassName() );
+        throwable.put( "message", throwableProxy.getMessage() );
+
+        if ( null != throwableProxy.getStackTraceElementProxyArray() && throwableProxy.getStackTraceElementProxyArray
+            ().length > 0 )
+        {
+            JSONArray array = new JSONArray();
+            for ( StackTraceElementProxy ste : throwableProxy.getStackTraceElementProxyArray() )
+            {
+                array.put( ste.getSTEAsString() );
+            }
+            throwable.put( "stackTrace", array );
+        }
+
+        return throwable;
     }
 
     public void addUri( String uri )
--- appender/src/test/java/com/github/nmorel/couchlog/LogGenerator.java
+++ appender/src/test/java/com/github/nmorel/couchlog/LogGenerator.java
@@ -1,10 +1,12 @@
 package com.github.nmorel.couchlog;
 
+import java.math.BigDecimal;
 import java.util.Random;
 
 import ch.qos.logback.classic.LoggerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 public class LogGenerator
 {
@@ -16,26 +18,38 @@ public class LogGenerator
 
         long start = System.currentTimeMillis();
 
-        for ( int i = 0; i < 5000; i++ )
+        try
+        {
+            new BigDecimal( (String) null );
+        }
+        catch ( Exception e )
+        {
+            logger.error( "Error while creating a BigDecimal", e );
+        }
+
+        MDC.put( "user", "Nicolas" );
+
+        for ( int i = 0; i < 50; i++ )
         {
             int level = rnd.nextInt( 5 );
-            String message = "Message #" + padStart( Integer.toString( i ), 4, '0' );
+            String message = "Message #{}";
+            Object param = padStart( Integer.toString( i ), 4, '0' );
             switch ( level )
             {
                 case 0:
-                    logger.trace( message );
+                    logger.trace( message, param );
                     break;
                 case 1:
-                    logger.debug( message );
+                    logger.debug( message, param );
                     break;
                 case 2:
-                    logger.info( message );
+                    logger.info( message, param );
                     break;
                 case 3:
-                    logger.warn( message );
+                    logger.warn( message, param );
                     break;
                 case 4:
-                    logger.error( message );
+                    logger.error( message, param );
                     break;
                 default:
                     break;
@@ -52,15 +66,18 @@ public class LogGenerator
     /**
      * Copy of the guava Strings.padStart. Don't want to add dependencies to guava just for one method used in test
      */
-    private static String padStart(String string, int minLength, char padChar) {
-        if (string.length() >= minLength) {
+    private static String padStart( String string, int minLength, char padChar )
+    {
+        if ( string.length() >= minLength )
+        {
             return string;
         }
-        StringBuilder sb = new StringBuilder(minLength);
-        for (int i = string.length(); i < minLength; i++) {
-            sb.append(padChar);
+        StringBuilder sb = new StringBuilder( minLength );
+        for ( int i = string.length(); i < minLength; i++ )
+        {
+            sb.append( padChar );
         }
-        sb.append(string);
+        sb.append( string );
         return sb.toString();
     }
 }
